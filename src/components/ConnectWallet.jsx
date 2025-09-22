af9bd0fd35d18ec165c2a760541b85a6a464a64c
web3senior <atenyun@gmail.com>
2025-09-22 19:53:24 +0330

    Add connectWalletModal

--- src/components/ConnectWallet.jsx
+++ src/components/ConnectWallet.jsx
@@ -8,6 +8,7 @@ import Shimmer from '@/helper/Shimmer'
 import styles from './ConnectWallet.module.scss'
 
 export const ConnectWallet = () => {
+  const [showModal, setShowModal] = useState(true)
   const { disconnect } = useDisconnect()
   const mounted = useClientMounted()
   const { address, isConnected } = useAccount()
@@ -24,14 +25,12 @@ export const ConnectWallet = () => {
     <div>
       {!isConnected ? (
         <>
-          <button className={`flex align-items-center gap-025 `} onClick={() => open()}>
+          <button className={`flex align-items-center gap-025 `} onClick={() => setShowModal(true)}>
             <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#1f1f1f">
               <path d="M224.62-160q-27.62 0-46.12-18.5Q160-197 160-224.62v-510.76q0-27.62 18.5-46.12Q197-800 224.62-800h510.76q27.62 0 46.12 18.5Q800-763 800-735.38V-680H544.62q-47.93 0-76.27 28.35Q440-623.31 440-575.38v190.76q0 47.93 28.35 76.27Q496.69-280 544.62-280H800v55.38q0 27.62-18.5 46.12Q763-160 735.38-160H224.62Zm320-160q-27.62 0-46.12-18.5Q480-357 480-384.62v-190.76q0-27.62 18.5-46.12Q517-640 544.62-640h230.76q27.62 0 46.12 18.5Q840-603 840-575.38v190.76q0 27.62-18.5 46.12Q803-320 775.38-320H544.62ZM640-420q26 0 43-17t17-43q0-26-17-43t-43-17q-26 0-43 17t-17 43q0 26 17 43t43 17Z" />
             </svg>
             Connect Wallet
           </button>
-
-          <WalletOptions />
         </>
       ) : (
         <div className={`flex gap-050`}>
@@ -39,9 +38,15 @@ export const ConnectWallet = () => {
           <button onClick={() => disconnect()}>Disconnect</button>
         </div>
       )}
+      {showModal && <WalletConnectModal setShowModal={setShowModal} />}
+    </div>
+  )
+}
 
-      {/* <button onClick={handleDisconnect}>Disconnect</button>
-        <button onClick={() => switchNetwork(networks[1]) }>Switch</button> */}
+export function WalletConnectModal({ setShowModal }) {
+  return (
+    <div className={`${styles.walletConnectModal}`} onClick={() => setShowModal(false)}>
+      <WalletOptions />
     </div>
   )
 }
@@ -49,8 +54,10 @@ export const ConnectWallet = () => {
 export function WalletOptions() {
   const { connectors, connect } = useConnect()
 
+  const handleConnect = () => {}
+
   return connectors.map((connector) => (
-    <button key={connector.uid} onClick={() => connect({ connector })}>
+    <button className={`${styles['wallet']}`} key={connector.uid} onClick={() => connect({ connector })}>
       {connector.name}
     </button>
   ))
