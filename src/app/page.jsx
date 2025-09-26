bf661c21189985609d585c1e93525307a1c5fcde
web3senior <atenyun@gmail.com>
2025-09-26 19:52:43 +0330

    Change showMore color

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -145,95 +145,90 @@ export default function Page() {
   }, [])
 
   return (
-      <div className={`${styles.page} ms-motion-slideDownIn`}>
-        <h3 className={`page-title`}>home</h3>
+    <div className={`${styles.page} ms-motion-slideDownIn`}>
+      <h3 className={`page-title`}>home</h3>
 
-        <div className={`__container ${styles.page__container}`} data-width={`medium`}>
-          {polls.length < 1 && <div className={`shimmer ${styles.pollShimmer}`} />}
-          <div className={`${styles.grid} flex flex-column`}>
-            {polls &&
-              polls.length > 0 &&
-              polls.map((item, i) => {
-                return (
-                  <article key={i} className={`${styles.poll}`} onClick={() => router.push(`p/${item.pollId}`)}>
-                    <section data-name={item.name} className={`flex flex-column align-items-start justify-content-between`}>
-                      <header className={`${styles.poll__header}`}>
-                        <Profile creator={item.creator} createdAt={item.createdAt} />
-                      </header>
-                      <main className={`${styles.poll__main} w-100 flex flex-column grid--gap-050`}>
-                        <div onClick={(e) => e.stopPropagation()} className={`${styles.poll__question} `} 
-                        id={`pollQuestion${item.pollId}`} dangerouslySetInnerHTML={{__html: `<p>${item.question}</p>`}}/>
+      <div className={`__container ${styles.page__container}`} data-width={`medium`}>
+        {polls.length < 1 && <div className={`shimmer ${styles.pollShimmer}`} />}
+        <div className={`${styles.grid} flex flex-column`}>
+          {polls &&
+            polls.length > 0 &&
+            polls.map((item, i) => {
+              return (
+                <article key={i} className={`${styles.poll}`} onClick={() => router.push(`p/${item.pollId}`)}>
+                  <section data-name={item.name} className={`flex flex-column align-items-start justify-content-between`}>
+                    <header className={`${styles.poll__header}`}>
+                      <Profile creator={item.creator} createdAt={item.createdAt} />
+                    </header>
+                    <main className={`${styles.poll__main} w-100 flex flex-column grid--gap-050`}>
+                      <div onClick={(e) => e.stopPropagation()} className={`${styles.poll__question} `} id={`pollQuestion${item.pollId}`} dangerouslySetInnerHTML={{ __html: `<p>${item.question}</p>` }} />
 
-                                  {item.question.length > 150 ? (
-                            <>
-                              <button
-                                className={`${styles.poll__showmore}`}
-                                onClick={(e) => {
-                                  e.stopPropagation()
-                                  document.querySelector(`#pollQuestion${item.pollId}`).style.maxHeight = `unset !important`
-                                  e.target.remove()
-                                }}
-                              >
-                                <b className={`text-primary`}>Show More</b>
-                              </button>
-                            </>
-                          ) : (
-                            <></>
+                      {item.question.length > 150 && (
+                        <button
+                          className={`${styles.poll__btnShowMore}`}
+                          onClick={(e) => {
+                            e.stopPropagation()
+                            document.querySelector(`#pollQuestion${item.pollId}`).style.maxHeight = `unset !important`
+                            e.target.remove()
+                          }}
+                        >
+                          <b className={`text-primary`}>Show More</b>
+                        </button>
+                      )}
+
+                      {/* Is it poll or a post? */}
+                      {item.options.length > 0 && (
+                        <>
+                          {item.pollType.toString() === `2` && (
+                            <div className={`flex flex-row align-items-center gap-025`}>
+                              <span className={`badge badge-pill badge-primary`}>only lyx holders</span>
+                              <span className={`badge badge-pill badge-danger`}>&gt; {web3.utils.fromWei(item.holderAmount, `ether`)} LYX</span>
+                            </div>
                           )}
+                          <Options item={item} />
+                        </>
+                      )}
 
-                        {/* Is it poll or a post? */}
-                        {item.options.length > 0 && (
-                          <>
-                            {item.pollType.toString() === `2` && (
-                              <div className={`flex flex-row align-items-center gap-025`}>
-                                <span className={`badge badge-pill badge-primary`}>only lyx holders</span>
-                                <span className={`badge badge-pill badge-danger`}>&gt; {web3.utils.fromWei(item.holderAmount, `ether`)} LYX</span>
-                              </div>
-                            )}
-                            <Options item={item} />
-                          </>
+                      <div onClick={(e) => e.stopPropagation()} className={`${styles.poll__actions} flex flex-row align-items-center justify-content-start`}>
+                        <button onClick={(e) => likePoll(e, item.pollId)}>{<LikeCount pollId={item.pollId} />}</button>
+
+                        {item.allowedComments && (
+                          <button>
+                            <img alt={`blue checkmark icon`} src={commentIcon.src} />
+                            <span>{0}</span>
+                          </button>
                         )}
 
-                        <div onClick={(e) => e.stopPropagation()} className={`${styles.poll__actions} flex flex-row align-items-center justify-content-start`}>
-                          <button onClick={(e) => likePoll(e, item.pollId)}>{<LikeCount pollId={item.pollId} />}</button>
+                        <button>
+                          <img alt={`blue checkmark icon`} src={repostIcon.src} />
+                        </button>
 
-                          {item.allowedComments && (
-                            <button>
-                              <img alt={`blue checkmark icon`} src={commentIcon.src} />
-                              <span>{0}</span>
-                            </button>
-                          )}
+                        <button>
+                          <img alt={`blue checkmark icon`} src={shareIcon.src} />
+                        </button>
 
-                          <button>
-                            <img alt={`blue checkmark icon`} src={repostIcon.src} />
-                          </button>
-
-                          <button>
-                            <img alt={`blue checkmark icon`} src={shareIcon.src} />
-                          </button>
-
-                          <button>
-                            <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
-                              <path
-                                d="M12 8.16338C12.1836 8.16338 12.3401 8.09875 12.4695 7.9695C12.5988 7.84012 12.6634 7.68363 12.6634 7.5C12.6634 7.31638 12.5988 7.15988 12.4695 7.0305C12.3401 6.90125 12.1836 6.83663 12 6.83663C11.8164 6.83663 11.6599 6.90125 11.5305 7.0305C11.4013 7.15988 11.3366 7.31638 11.3366 7.5C11.3366 7.68363 11.4013 7.84012 11.5305 7.9695C11.6599 8.09875 11.8164 8.16338 12 8.16338ZM6 6.5625H9.75V5.4375H6V6.5625ZM3.65625 15.375C3.26013 14.0076 2.86425 12.6471 2.46863 11.2933C2.07288 9.93944 1.875 8.55 1.875 7.125C1.875 6.08075 2.23894 5.19469 2.96681 4.46681C3.69469 3.73894 4.58075 3.375 5.625 3.375H9.5625C9.90575 2.924 10.3176 2.56125 10.7979 2.28675C11.2782 2.01225 11.8039 1.875 12.375 1.875C12.5818 1.875 12.7584 1.94831 12.9051 2.09494C13.0517 2.24156 13.125 2.41825 13.125 2.625C13.125 2.676 13.118 2.72694 13.104 2.77781C13.0901 2.82881 13.0755 2.87594 13.0601 2.91919C12.9909 3.09994 12.9319 3.28506 12.8833 3.47456C12.8348 3.66394 12.7933 3.85525 12.7586 4.0485L14.7101 6H16.125V10.5821L14.0783 11.2543L12.8438 15.375H9.375V13.875H7.125V15.375H3.65625ZM4.5 14.25H6V12.75H10.5V14.25H12L13.1625 10.3875L15 9.76875V7.125H14.25L11.625 4.5C11.625 4.25 11.6406 4.00938 11.6719 3.77813C11.7031 3.54688 11.7548 3.31488 11.8269 3.08213C11.4644 3.18213 11.1481 3.35644 10.8778 3.60506C10.6077 3.85356 10.4005 4.15188 10.2563 4.5H5.625C4.9 4.5 4.28125 4.75625 3.76875 5.26875C3.25625 5.78125 3 6.4 3 7.125C3 8.35 3.16875 9.54688 3.50625 10.7156C3.84375 11.8844 4.175 13.0625 4.5 14.25Z"
-                                fill="#424242"
-                              />
-                            </svg>
-                            <span>{new Intl.NumberFormat().format(0)} LYX</span>
-                          </button>
-                          {/* <Link target={`_blank`} href={`https://exmaple.com/tx/`} className={`flex flex-row align-items-center gap-025  `}>
+                        <button>
+                          <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path
+                              d="M12 8.16338C12.1836 8.16338 12.3401 8.09875 12.4695 7.9695C12.5988 7.84012 12.6634 7.68363 12.6634 7.5C12.6634 7.31638 12.5988 7.15988 12.4695 7.0305C12.3401 6.90125 12.1836 6.83663 12 6.83663C11.8164 6.83663 11.6599 6.90125 11.5305 7.0305C11.4013 7.15988 11.3366 7.31638 11.3366 7.5C11.3366 7.68363 11.4013 7.84012 11.5305 7.9695C11.6599 8.09875 11.8164 8.16338 12 8.16338ZM6 6.5625H9.75V5.4375H6V6.5625ZM3.65625 15.375C3.26013 14.0076 2.86425 12.6471 2.46863 11.2933C2.07288 9.93944 1.875 8.55 1.875 7.125C1.875 6.08075 2.23894 5.19469 2.96681 4.46681C3.69469 3.73894 4.58075 3.375 5.625 3.375H9.5625C9.90575 2.924 10.3176 2.56125 10.7979 2.28675C11.2782 2.01225 11.8039 1.875 12.375 1.875C12.5818 1.875 12.7584 1.94831 12.9051 2.09494C13.0517 2.24156 13.125 2.41825 13.125 2.625C13.125 2.676 13.118 2.72694 13.104 2.77781C13.0901 2.82881 13.0755 2.87594 13.0601 2.91919C12.9909 3.09994 12.9319 3.28506 12.8833 3.47456C12.8348 3.66394 12.7933 3.85525 12.7586 4.0485L14.7101 6H16.125V10.5821L14.0783 11.2543L12.8438 15.375H9.375V13.875H7.125V15.375H3.65625ZM4.5 14.25H6V12.75H10.5V14.25H12L13.1625 10.3875L15 9.76875V7.125H14.25L11.625 4.5C11.625 4.25 11.6406 4.00938 11.6719 3.77813C11.7031 3.54688 11.7548 3.31488 11.8269 3.08213C11.4644 3.18213 11.1481 3.35644 10.8778 3.60506C10.6077 3.85356 10.4005 4.15188 10.2563 4.5H5.625C4.9 4.5 4.28125 4.75625 3.76875 5.26875C3.25625 5.78125 3 6.4 3 7.125C3 8.35 3.16875 9.54688 3.50625 10.7156C3.84375 11.8844 4.175 13.0625 4.5 14.25Z"
+                              fill="#424242"
+                            />
+                          </svg>
+                          <span>{new Intl.NumberFormat().format(0)} LYX</span>
+                        </button>
+                        {/* <Link target={`_blank`} href={`https://exmaple.com/tx/`} className={`flex flex-row align-items-center gap-025  `}>
                           <img alt={`blue checkmark icon`} src={txIcon.src} />
                         </Link> */}
-                        </div>
-                      </main>
-                    </section>
-                    {i < polls.length - 1 && <hr />}
-                  </article>
-                )
-              })}
-          </div>
+                      </div>
+                    </main>
+                  </section>
+                  {i < polls.length - 1 && <hr />}
+                </article>
+              )
+            })}
         </div>
       </div>
+    </div>
   )
 }
 
