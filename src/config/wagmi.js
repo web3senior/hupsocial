ad19f86658399aaff78ae8c7ec406e06a1e81dc8
web3senior <atenyun@gmail.com>
2025-10-01 23:02:50 +0330

    Add more chains

--- src/config/wagmi.js
+++ src/config/wagmi.js
@@ -1,14 +1,16 @@
 import { http, createConfig } from 'wagmi'
-import { base, mainnet, optimism, lukso, luksoTestnet } from 'wagmi/chains'
+import { base,arbitrumGoerli, somniaTestnet, mainnet, optimism, lukso, luksoTestnet } from 'wagmi/chains'
 import { injected, metaMask, safe, walletConnect } from 'wagmi/connectors'
 
 const projectId = process.env.NEXT_PUBLIC_PROJECT_ID || ``
 
 export const config = createConfig({
-  chains: [luksoTestnet],//mainnet, base, lukso, 
+  chains: [luksoTestnet, arbitrumGoerli, somniaTestnet],//mainnet, base, lukso, 
   connectors: [injected(), walletConnect({ projectId }), metaMask()], //, safe()
   transports: {
     [luksoTestnet.id]: http(),
+     [arbitrumGoerli.id]: http(),
+     [somniaTestnet.id]: http(),
     // [mainnet.id]: http(),
     // [base.id]: http(),
     // [lukso.id]: http(),
