7c107ff1f4d9137b1c2c27c984bfd1e8ab3a5275
web3senior <atenyun@gmail.com>
2025-10-09 15:22:46 +0330

    Fix connect btn

--- src/components/ConnectWallet.jsx
+++ src/components/ConnectWallet.jsx
@@ -34,7 +34,7 @@ export const ConnectWallet = () => {
   return !mounted ? null : (
     <>
       {!isConnected && (
-        <button className={`flex align-items-center gap-025 `} onClick={() => setShowModal(true)}>
+        <button className={`${styles.btnConnect} flex align-items-center gap-025 `} onClick={() => setShowModal(true)}>
           <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#fff">
             <path d="M224.62-160q-27.62 0-46.12-18.5Q160-197 160-224.62v-510.76q0-27.62 18.5-46.12Q197-800 224.62-800h510.76q27.62 0 46.12 18.5Q800-763 800-735.38V-680H544.62q-47.93 0-76.27 28.35Q440-623.31 440-575.38v190.76q0 47.93 28.35 76.27Q496.69-280 544.62-280H800v55.38q0 27.62-18.5 46.12Q763-160 735.38-160H224.62Zm320-160q-27.62 0-46.12-18.5Q480-357 480-384.62v-190.76q0-27.62 18.5-46.12Q517-640 544.62-640h230.76q27.62 0 46.12 18.5Q840-603 840-575.38v190.76q0 27.62-18.5 46.12Q803-320 775.38-320H544.62ZM640-420q26 0 43-17t17-43q0-26-17-43t-43-17q-26 0-43 17t-17 43q0 26 17 43t43 17Z" />
           </svg>
@@ -42,8 +42,8 @@ export const ConnectWallet = () => {
         </button>
       )}
 
-    
-        {isConnected && chains && defaultChain && (
+      {isConnected && chains && defaultChain && (
+        <>
           <div className={`${styles.networks}`}>
             <button onClick={(e) => setShowNetworks(!showNetworks)} title={`${defaultChain.name}`}>
               <span className={`rounded`} dangerouslySetInnerHTML={{ __html: defaultChain.icon }} />
@@ -56,9 +56,8 @@ export const ConnectWallet = () => {
               <div className={`${styles.dropdown} animate fade flex flex-column align-items-center justify-content-start gap-050`}>
                 <ul>
                   {chains.map((item, i) => (
-                    <li key={i} onClick={() => setShowNetworks(false)}
-                    disabled={item.name !== `LUKSO Testnet`} className={`flex flex-row align-items-center justify-content-start gap-050`} disabled={item.name !== `LUKSO Testnet`}>
-                      <figure className={`rounded`}  dangerouslySetInnerHTML={{ __html: item.icon }} />
+                    <li key={i} onClick={() => setShowNetworks(false)} disabled={item.name !== `LUKSO Testnet`} className={`flex flex-row align-items-center justify-content-start gap-050`} disabled={item.name !== `LUKSO Testnet`}>
+                      <figure className={`rounded`} dangerouslySetInnerHTML={{ __html: item.icon }} />
                       <small>{item.name}</small>
                     </li>
                   ))}
@@ -66,10 +65,10 @@ export const ConnectWallet = () => {
               </div>
             )}
           </div>
-        )}
 
-        <Profile addr={address} />
-
+          <Profile addr={address} />
+        </>
+      )}
 
       {showModal && <WalletConnectModal setShowModal={setShowModal} />}
     </>
