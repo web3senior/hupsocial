0b83de5ee07b863d89f2330ec6cfe7967bac95db
web3senior <atenyun@gmail.com>
2025-11-22 16:52:42 +0330

    Add dynamic open graph

---
+++ src/app/[chainId]/p/[id]/opengraph-image.jsx
@@ -0,0 +1,44 @@
+import { ImageResponse } from 'next/og'
+import { getPostByIndex } from '@/util/communication'
+ 
+// Image metadata
+export const size = {
+  width: 1200,
+  height: 630,
+}
+ 
+export const contentType = 'image/png'
+ 
+// Image generation
+export default async function Image({ params }) {
+  const post = await getPostByIndex(params.id)
+
+if (!post) {
+  return new ImageResponse(
+    // Default error image JSX
+    <div style={{color:`red`}}>Post Not Found</div>
+  );
+}
+
+
+  post.postId = params.id
+
+  return new ImageResponse(
+    (
+      // ImageResponse JSX element
+      <div
+        style={{
+          fontSize: 128,
+          background: 'white',
+          width: '100%',
+          height: '100%',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+      >
+        {post.postId}
+      </div>
+    )
+  )
+}
