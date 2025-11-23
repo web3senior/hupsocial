1bd9c91050dc7813e386004ccda7e486d78e01f0
web3senior <atenyun@gmail.com>
2025-11-24 00:19:40 +0330

    Allow embed Hup as an IFRAME

--- next.config.mjs
+++ next.config.mjs
@@ -10,10 +10,10 @@ const nextConfig = {
             key: 'X-Content-Type-Options',
             value: 'nosniff',
           },
-          {
-            key: 'X-Frame-Options',
-            value: 'DENY',
-          },
+          // {
+          //   key: 'X-Frame-Options',
+          //   value: 'DENY',
+          // },
           {
             key: 'Referrer-Policy',
             value: 'strict-origin-when-cross-origin',
