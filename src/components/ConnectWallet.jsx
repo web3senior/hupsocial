3be7d862f31371ab7a3a97ae50dfedbb09fc54e6
web3senior <atenyun@gmail.com>
2025-11-24 23:17:21 +0330

    Optimize switchChain func

--- src/components/ConnectWallet.jsx
+++ src/components/ConnectWallet.jsx
@@ -190,7 +190,6 @@ export default function DefaultNetwork({ currentNetwork, setShowNetworks }) {
           },
           onError: (error) => {
             console.error('Switch chain failed:', error)
-            // Error logic
           },
         }
       )
@@ -201,45 +200,41 @@ export default function DefaultNetwork({ currentNetwork, setShowNetworks }) {
 
   useEffect(() => {
     // networkDialog.current.showModal()
-
     networkDialog.current.addEventListener('close', (e) => {
       const returnValue = networkDialog.current.returnValue
       if (returnValue === `close`) return
-
-      localStorage.setItem(`${process.env.NEXT_PUBLIC_LOCALSTORAGE_PREFIX}active-chain`, returnValue)
       handleSwitchChain(returnValue)
-      // setShowNetworks(false)
       // networkDialog.current.close()
     })
   }, [])
 
   return (
     <dialog ref={networkDialog} id={`networkDialog`} className={`dialog ${styles.networkDialog} `}>
-        <h2>Select Your Network</h2>
-        <p>Your choices shape the content you experience. Each network carries a unique, unalterable history of posts, identities, and governance votes.</p>
+      <h2>Select Your Network</h2>
+      <p>Your choices shape the content you experience. Each network carries a unique, unalterable history of posts, identities, and governance votes.</p>
 
-        <form method={`dialog`}>
-          <div className={`${styles.networks} grid grid--fit gap-050`} style={{ '--data-width': `150px` }}>
-            {config.chains.map((chain, i) => (
-              <button
-                key={i}
-                onClick={(e) => {
-                  e.preventDefault()
-                  networkDialog.current.close(chain.id)
-                }}
-                data-current={chain.id.toString() === currentNetwork.toString()}
-              >
-                <div className={`rounded`} dangerouslySetInnerHTML={{ __html: chain.icon }} />
-                <span>{chain.name}</span>
-              </button>
-            ))}
-          </div>
-          <button className={`close`} value={`close`}>
-            <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#1f1f1f">
-              <path d="m322.15-293.08-29.07-29.07L450.92-480 293.08-636.85l29.07-29.07L480-508.08l156.85-157.84 29.07 29.07L508.08-480l157.84 157.85-29.07 29.07L480-450.92 322.15-293.08Z" />
-            </svg>
-          </button>
-        </form>
+      <form method={`dialog`}>
+        <div className={`${styles.networks} grid grid--fit gap-050`} style={{ '--data-width': `150px` }}>
+          {config.chains.map((chain, i) => (
+            <button
+              key={i}
+              onClick={(e) => {
+                e.preventDefault()
+                networkDialog.current.close(chain.id)
+              }}
+              data-current={chain.id.toString() === currentNetwork.toString()}
+            >
+              <div className={`rounded`} dangerouslySetInnerHTML={{ __html: chain.icon }} />
+              <span>{chain.name}</span>
+            </button>
+          ))}
+        </div>
+        <button className={`close`} value={`close`}>
+          <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#1f1f1f">
+            <path d="m322.15-293.08-29.07-29.07L450.92-480 293.08-636.85l29.07-29.07L480-508.08l156.85-157.84 29.07 29.07L508.08-480l157.84 157.85-29.07 29.07L480-450.92 322.15-293.08Z" />
+          </svg>
+        </button>
+      </form>
     </dialog>
   )
 }
