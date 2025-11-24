bc81f84bd7e3e00148ed04a70c1b71abfb12b0c3
web3senior <atenyun@gmail.com>
2025-11-24 15:41:36 +0330

    Fix open network modal

--- src/components/ConnectWallet.jsx
+++ src/components/ConnectWallet.jsx
@@ -1,16 +1,16 @@
 'use client'
 
 import Link from 'next/link'
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useRef } from 'react'
 import { useClientMounted } from '@/hooks/useClientMount'
 import { config } from '@/config/wagmi'
-import { useAccount, useDisconnect, Connector, useConnect } from 'wagmi'
-import { useSwitchChain, useConfig } from 'wagmi'
+import { useAccount, useDisconnect, Connector, useConnect, useSwitchChain, useConfig  } from 'wagmi'
 import { getActiveChain } from '@/util/communication'
 import { getProfile, getUniversalProfile } from '@/util/api'
 import Shimmer from '@/helper/Shimmer'
 import styles from './ConnectWallet.module.scss'
-import DefaultNetwork from './DefaultNetwork'
+
+
 
 export const ConnectWallet = () => {
   const [showModal, setShowModal] = useState(false)
@@ -58,11 +58,13 @@ export const ConnectWallet = () => {
       {activeChain[0] && (
         <>
           <div className={`${styles.networks}`}>
-            <button className={`${styles.btnNetwork}`} onClick={(e) => setShowNetworks(!showNetworks)} title={`${activeChain[0].name}`}>
+            <button className={`${styles.btnNetwork}`} onClick={(e) => {
+              document.querySelector(`#networkDialog`).showModal()
+            }} title={`${activeChain[0].name}`}>
               <span className={`rounded`} dangerouslySetInnerHTML={{ __html: activeChain[0].icon }} />
             </button>
 
-            {showNetworks && <DefaultNetwork currentNetwork={activeChain[0].id} setShowNetworks={setShowNetworks} />}
+           <DefaultNetwork currentNetwork={activeChain[0].id} />
             {/* {showNetworks && (
               <div className={`${styles.dropdown} animate fade flex flex-column align-items-center justify-content-start gap-050`}>
                 <ul>
@@ -165,5 +167,75 @@ const Profile = ({ addr }) => {
         <img alt={data.name} src={`${data.profileImage}`} className={`rounded`} />
       </figure>
     </Link>
+  )
+}
+
+export default function DefaultNetwork({ currentNetwork, setShowNetworks }) {
+  const networkDialog = useRef()
+  const { address, isConnected } = useAccount()
+  const { switchChain } = useSwitchChain()
+
+  const handleSwitchChain = (chainId) => {
+    console.log(`Switch network: `, chainId)
+    if (isConnected) {
+      switchChain(
+        { chainId: parseInt(chainId) },
+        {
+          onSuccess: () => {
+            localStorage.setItem(`${process.env.NEXT_PUBLIC_LOCALSTORAGE_PREFIX}active-chain`, chainId)
+            window.location.href = '/'
+          },
+          onError: (error) => {
+            console.error('Switch chain failed:', error)
+            // Error logic
+          },
+        }
+      )
+    } else {
+      localStorage.setItem(`${process.env.NEXT_PUBLIC_LOCALSTORAGE_PREFIX}active-chain`, chainId)
+    }
+  }
+
+  useEffect(() => {
+    // networkDialog.current.showModal()
+
+    networkDialog.current.addEventListener('close', (e) => {
+      const returnValue = networkDialog.current.returnValue
+      if (returnValue === `close`) return
+
+      localStorage.setItem(`${process.env.NEXT_PUBLIC_LOCALSTORAGE_PREFIX}active-chain`, returnValue)
+      handleSwitchChain(returnValue)
+      // setShowNetworks(false)
+      // networkDialog.current.close()
+    })
+  }, [])
+
+  return (
+    <dialog ref={networkDialog} id={`networkDialog`} className={`${styles.networkDialog}`}>
+      <h2>Select Your Hup Network</h2>
+      <p>Your choice determines the content you see. Each network holds a separate, immutable history of posts, identities, and governance votes.</p>
+      <form method={`dialog`} className={`mt-20`}>
+        <div className={`${styles.networks} grid grid--fit gap-1`} style={{ '--data-width': `150px` }}>
+          {config.chains.map((chain, i) => (
+            <button
+              key={i}
+              onClick={(e) => {
+                e.preventDefault()
+                networkDialog.current.close(chain.id)
+              }}
+              data-current={chain.id.toString() === currentNetwork.toString()}
+            >
+              <div className={`rounded`} dangerouslySetInnerHTML={{ __html: chain.icon }} />
+              <span>{chain.name}</span>
+            </button>
+          ))}
+        </div>
+        <button value={`close`} action="close">
+          <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#1f1f1f">
+            <path d="m322.15-293.08-29.07-29.07L450.92-480 293.08-636.85l29.07-29.07L480-508.08l156.85-157.84 29.07 29.07L508.08-480l157.84 157.85-29.07 29.07L480-450.92 322.15-293.08Z" />
+          </svg>
+        </button>
+      </form>
+    </dialog>
   )
 }
--- src/config/wagmi.js
+++ src/config/wagmi.js
@@ -1,6 +1,5 @@
 import { http, createConfig } from 'wagmi'
-import { base, baseSepoliaPreconf, celoSepolia, opBNBTestnet, arbitrumSepolia, 
-  monadTestnet, somniaTestnet, mainnet, optimismSepolia, lukso, luksoTestnet, lineaSepolia } from 'wagmi/chains'
+import { base, baseSepoliaPreconf, celoSepolia, opBNBTestnet, arbitrumSepolia, monadTestnet, somniaTestnet, mainnet, unichainSepolia, optimismSepolia, lukso, luksoTestnet, lineaSepolia } from 'wagmi/chains'
 import { injected, metaMask, safe, walletConnect } from 'wagmi/connectors'
 
 const projectId = process.env.NEXT_PUBLIC_PROJECT_ID || ``
@@ -36,6 +35,11 @@ export const CONTRACTS = {
     comment: '0xc407722d150c8a65e890096869f8015D90a89EfD',
     status: '0xA5e73b15c1C3eE477AED682741f0324C6787bbb8',
   },
+  chain1301: {
+    post: '0xf5e4d19c9de1323dfF4fd85822Ca7A3582035e76',
+    comment: '0x4E6Bab4961Ab53D70745E791FA727993A4221d1F',
+    status: '0xc407722d150c8a65e890096869f8015D90a89EfD',
+  },
 }
 
 // Expand the properties
@@ -66,6 +70,9 @@ lineaSepolia.icon = `<svg width="18" height="18" viewBox="0 0 18 18" fill="none"
 // OptimismSepolia
 optimismSepolia.icon = `<svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg"><g clip-path="url(#clip0_10964_3437)"><path d="M18 0H0V18H18V0Z" fill="#FE0420"/><path fill-rule="evenodd" clip-rule="evenodd" d="M3.64398 11.5333C4.07903 11.8444 4.63715 12 5.31834 12C6.14264 12 6.80091 11.8139 7.29322 11.4417C7.78551 11.0639 8.13181 10.4944 8.33218 9.73332C8.45238 9.26667 8.55541 8.78611 8.6413 8.29167C8.66992 8.11389 8.68421 7.96667 8.68421 7.85C8.68421 7.46111 8.58403 7.12778 8.38369 6.85C8.18333 6.56667 7.90858 6.35556 7.5594 6.21667C7.21021 6.07222 6.81524 6 6.37447 6C4.75449 6 3.74988 6.76389 3.36063 8.29167C3.22325 8.85278 3.11735 9.33333 3.04293 9.73332C3.01431 9.9111 3 10.0611 3 10.1833C3 10.7667 3.21466 11.2167 3.64398 11.5333ZM6.74367 9.68334C6.5793 10.3215 6.12578 10.7368 5.43855 10.7368C4.75869 10.7368 4.52622 10.2771 4.64859 9.68334C4.75163 9.14445 4.85467 8.68889 4.95771 8.31667C5.13523 7.62748 5.55191 7.26316 6.26284 7.26316C6.93965 7.26316 7.16201 7.71644 7.04422 8.31667C6.9755 8.70556 6.87535 9.1611 6.74367 9.68334ZM9.34562 11.94C9.37902 11.98 9.42633 12 9.48761 12H10.6235C10.6792 12 10.732 11.98 10.7822 11.94C10.8323 11.9 10.8629 11.8486 10.874 11.7857L11.2693 10H12.425C13.1544 10 13.6887 9.75144 14.1063 9.43716C14.5295 9.12285 14.8107 8.63714 14.9499 7.98C14.9833 7.82571 15 7.67714 15 7.53429C15 7.03714 14.8107 6.65714 14.432 6.39429C14.059 6.13143 13.5634 6 12.9453 6H10.7237C10.668 6 10.6151 6.02 10.565 6.06C10.5149 6.1 10.4843 6.15143 10.4731 6.21429L9.32055 11.7857C9.30941 11.8429 9.31778 11.8943 9.34562 11.94ZM13.4047 7.96286C13.3008 8.4217 12.9369 8.74352 12.4671 8.74352H11.5066L11.8011 7.26316H12.8034C13.1446 7.26316 13.4298 7.33041 13.4298 7.70571C13.4298 7.78 13.4214 7.86571 13.4047 7.96286Z" fill="white"/></g><defs><clipPath id="clip0_10964_3437"><rect width="18" height="18" fill="white"/></clipPath></defs></svg>`
 
+// Unichain
+unichainSepolia.icon = `<svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg"><g clip-path="url(#clip0_10964_3441)"><path d="M18 0H0V18H18V0Z" fill="#F50DB4"/><path d="M14.9742 8.88585C11.729 8.88585 9.10099 6.24956 9.10099 3H8.87318V8.88585H3V9.11415C6.24521 9.11415 8.87318 11.7505 8.87318 15H9.10099V9.11415H14.9742V8.88585Z" fill="white"/></g><defs><clipPath id="clip0_10964_3441"><rect width="18" height="18" fill="white"/></clipPath></defs></svg>`
+
 // Arbitrum
 arbitrumSepolia.icon = `<svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg"> <g clip-path="url(#clip0_10733_2639)"> <path d="M0 0H18V18H0V0Z" fill="#213147"/> <path d="M9.84015 9.91092L9.29015 11.5109C9.27568 11.553 9.27568 11.5988 9.29015 11.6409L10.2302 14.3809L11.3202 13.7209L10.0202 9.92092C10.012 9.90401 9.99918 9.88975 9.98326 9.87977C9.96735 9.8698 9.94894 9.8645 9.93015 9.8645C9.91137 9.8645 9.89296 9.8698 9.87705 9.87977C9.86113 9.88975 9.84834 9.90401 9.84015 9.92092V9.91092ZM10.9302 7.23092C10.9212 7.21644 10.9087 7.2045 10.8938 7.19622C10.8789 7.18794 10.8622 7.18359 10.8452 7.18359C10.8281 7.18359 10.8114 7.18794 10.7965 7.19622C10.7816 7.2045 10.7691 7.21644 10.7602 7.23092L10.2102 8.83092C10.1957 8.87304 10.1957 8.91879 10.2102 8.96092L11.7502 13.4609L12.8402 12.7809L10.9402 7.23092H10.9302Z" fill="#12AAFF"/> <path d="M9 3.67C9.03 3.67 9.05 3.67 9.08 3.69L13.3 6.31C13.35 6.34 13.38 6.39 13.38 6.45V11.55C13.38 11.6 13.35 11.65 13.3 11.69L9.08 14.3C9.05605 14.3151 9.02831 14.3231 9 14.3231C8.97169 14.3231 8.94395 14.3151 8.92 14.3L4.7 11.7C4.67612 11.6851 4.65631 11.6645 4.64235 11.6401C4.62838 11.6157 4.62071 11.5881 4.62 11.56V6.46C4.62 6.4 4.65 6.36 4.7 6.32L8.92 3.7C8.94289 3.68191 8.97086 3.67142 9 3.67ZM9 3C8.85 3 8.7 3.04 8.57 3.12L4.43 5.67C4.29815 5.75297 4.18953 5.86807 4.11432 6.0045C4.03911 6.14092 3.99977 6.29421 4 6.45V11.55C4 11.87 4.16 12.17 4.43 12.33L8.57 14.88C8.69889 14.9607 8.84791 15.0036 9 15.0036C9.15209 15.0036 9.30111 14.9607 9.43 14.88L13.57 12.33C13.7033 12.2461 13.8128 12.1295 13.8881 11.9911C13.9634 11.8528 14.0019 11.6975 14 11.54V6.45C13.9993 6.29439 13.9596 6.14145 13.8844 6.00518C13.8093 5.8689 13.7012 5.75366 13.57 5.67L9.43 3.12C9.30032 3.04148 9.1516 2.99998 9 3Z" fill="#9DCCED"/> <path d="M6.25977 13.4596L6.63977 12.3496L7.40977 13.0196L6.68977 13.7196L6.25977 13.4596Z" fill="#213147"/> <path d="M8.64992 6H7.59992C7.56099 6.00414 7.52414 6.01961 7.49392 6.04449C7.4637 6.06938 7.44145 6.10259 7.42992 6.14L5.16992 12.78L6.25992 13.46L8.73992 6.14C8.75992 6.07 8.71992 6 8.64992 6ZM10.4999 6H9.42992C9.39181 6.00208 9.3552 6.0156 9.32488 6.03879C9.29455 6.06198 9.27192 6.09376 9.25992 6.13L6.67992 13.71L7.76992 14.39L10.5699 6.14C10.5899 6.07 10.5499 6 10.4799 6H10.4999Z" fill="white"/> </g> <defs> <clipPath id="clip0_10733_2639"> <rect width="18" height="18" fill="white"/> </clipPath> </defs> </svg>`
 
@@ -76,7 +83,7 @@ somniaTestnet.icon = `<svg width="18" height="18" viewBox="0 0 18 18" fill="none
 opBNBTestnet.icon = `<svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg"> <g clip-path="url(#clip0_10769_2638)"> <path d="M0 0H18V18H0V0Z" fill="url(#paint0_linear_10769_2638)"/> <path d="M5.93 4.84L9 3L12.07 4.84L10.94 5.52L9 4.36L7.06 5.52L5.93 4.84ZM12.07 7.16L10.94 6.48L9 7.64L7.06 6.48L5.93 7.16V8.5L7.87 9.66V12L9 12.68L10.13 12V9.67L12.07 8.51V7.16ZM12.07 10.83V9.47L10.94 10.15V11.51L12.07 10.83ZM12.87 11.31L10.93 12.47V13.83L14 11.99V8.32L12.87 8.99V11.31ZM11.74 6L12.87 6.68V8.03L14 7.36V6L12.87 5.32L11.74 6ZM7.87 12.96V14.32L9 15L10.13 14.32V12.96L9 13.64L7.87 12.96ZM5.93 10.83L7.06 11.51V10.15L5.93 9.47V10.83ZM7.87 6L9 6.68L10.13 6L9 5.32L7.87 6ZM5.13 6.68L6.26 6L5.13 5.32L4 6V7.36L5.13 8.03V6.68ZM5.13 8.99L4 8.32V11.99L7.07 13.83V12.47L5.13 11.31V9V8.99Z" fill="#F0B90B"/> </g> <defs> <linearGradient id="paint0_linear_10769_2638" x1="3.35" y1="3.12" x2="21.9" y2="24.43" gradientUnits="userSpaceOnUse"> <stop stop-color="#1A1E21"/> <stop offset="1" stop-color="#06060A"/> </linearGradient> <clipPath id="clip0_10769_2638"> <rect width="18" height="18" fill="white"/> </clipPath> </defs> </svg>`
 
 export const config = createConfig({
-  chains: [baseSepoliaPreconf, luksoTestnet, celoSepolia, monadTestnet, lineaSepolia, optimismSepolia], //arbitrumSepolia,  , lineaSepolia, somniaTestnet, opBNBTestnet
+  chains: [baseSepoliaPreconf, luksoTestnet, celoSepolia, monadTestnet, lineaSepolia, optimismSepolia, unichainSepolia], //arbitrumSepolia,  , lineaSepolia, somniaTestnet, opBNBTestnet
   connectors: [walletConnect({ projectId }), metaMask()], //, safe() //injected(),
   transports: {
     [luksoTestnet.id]: http(),
@@ -85,6 +92,7 @@ export const config = createConfig({
     [lineaSepolia.id]: http(),
     [baseSepoliaPreconf.id]: http(),
     [optimismSepolia.id]: http(),
+    [unichainSepolia.id]: http(),
     // [arbitrumSepolia.id]: http(),
     // [somniaTestnet.id]: http(),
     // [opBNBTestnet.id]: http(),
