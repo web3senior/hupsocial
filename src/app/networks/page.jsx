249212cb702dfcc49092e4f994cb2b3744c398dc
web3senior <atenyun@gmail.com>
2025-11-30 15:31:38 +0330

    Remove shimmer

--- src/app/networks/page.jsx
+++ src/app/networks/page.jsx
@@ -1,5 +1,4 @@
 import Link from 'next/link'
-import React, { Suspense } from 'react'
 import { config } from '@/config/wagmi'
 import PageTitle from '@/components/PageTitle'
 import Shimmer from '@/components/ui/Shimmer'
@@ -12,35 +11,25 @@ export default function Page() {
       <div className={`${styles.page} ms-motion-slideDownIn`}>
         <div className={`__container ${styles.page__container}`} data-width={`medium`}>
           <div className={`flex flex-column gap-1`}>
-            {/* <Suspense fallback={<NetworksFallback />}> */}
-            <NetworkGrid />
-            {/* </Suspense> */}
+            {config.chains &&
+              config.chains.map((item, i) => {
+                return (
+                  <Link key={i} href={`./networks/${item.id}`} className={styles.button}>
+                    <div className={`${styles.network}`} title={`View details`}>
+                      <div className={`${styles.network__body} d-f-c flex-row justify-content-between gap-025`}>
+                        <div className={`flex flex-row align-items-center justify-content-start gap-050 flex-1`}>
+                          <div className={`${styles.network__icon}`} dangerouslySetInnerHTML={{ __html: item.icon }} />
+                          <span>{item.name}</span>
+                        </div>
+                        <small>View</small>
+                      </div>
+                    </div>
+                  </Link>
+                )
+              })}
           </div>
         </div>
       </div>
-    </>
-  )
-}
-
-const NetworkGrid = () => {
-  return (
-    <>
-      {config.chains &&
-        config.chains.map((item, i) => {
-          return (
-            <Link key={i} href={`networks/${item.id}`} className={styles.button}>
-              <div className={`${styles.network}`} title={`View details`}>
-                <div className={`${styles.network__body} d-f-c flex-row justify-content-between gap-025`}>
-                  <div className={`flex flex-row align-items-center justify-content-start gap-050 flex-1`}>
-                    <div className={`rounded ${styles.network__icon}`} dangerouslySetInnerHTML={{ __html: item.icon }} />
-                    <span>{item.name}</span>
-                  </div>
-                  <small>View</small>
-                </div>
-              </div>
-            </Link>
-          )
-        })}
     </>
   )
 }
