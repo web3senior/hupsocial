9331ab251e5887c90a3fce98db3d8621c5f3277b
web3senior <atenyun@gmail.com>
2025-12-02 04:28:23 +0330

    Update networks

--- src/app/networks/page.jsx
+++ src/app/networks/page.jsx
@@ -1,7 +1,7 @@
 import Link from 'next/link'
 import PageTitle from '@/components/PageTitle'
 import { config } from '@/config/wagmi'
-import {slugify} from '@/lib/utils'
+import { slugify } from '@/lib/utils'
 import styles from './page.module.scss'
 
 export default function Page() {
@@ -10,18 +10,17 @@ export default function Page() {
       <PageTitle name={`networks`} />
       <div className={`${styles.page} ms-motion-slideDownIn`}>
         <div className={`__container ${styles.page__container}`} data-width={`medium`}>
-          <div className={`flex flex-column gap-1`}>
+          <div className={`grid grid--fill gap-1`} style={{ '--data-width': `150px` }}>
             {config.chains &&
               config.chains.map((item, i) => {
                 return (
                   <Link key={i} href={`./networks/${item.id}/${slugify(item.name)}`} className={styles.button}>
                     <div className={`${styles.network}`} title={`View details`}>
                       <div className={`${styles.network__body} d-f-c flex-row justify-content-between gap-025`}>
-                        <div className={`flex flex-row align-items-center justify-content-start gap-050 flex-1`}>
+                        <div className={`flex flex-column align-items-center justify-content-start gap-050 flex-1`}>
                           <div className={`${styles.network__icon}`} dangerouslySetInnerHTML={{ __html: item.icon }} />
                           <span>{item.name}</span>
                         </div>
-                        <small>View</small>
                       </div>
                     </div>
                   </Link>
