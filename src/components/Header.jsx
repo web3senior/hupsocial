fc316148513c13c29e45a20b03ca01b577fd9cc2
web3senior <atenyun@gmail.com>
2025-12-20 19:51:27 +0330

    Change logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,7 @@
 import Link from 'next/link'
+import Image from 'next/image'
 import { ConnectWallet } from '@/components/ConnectWallet'
+import logo from '@/../public/hup.svg'
 import styles from './Header.module.scss'
 
 export default function Header() {
@@ -8,11 +10,7 @@ export default function Header() {
       <ul className={`flex align-items-center justify-content-between`}>
         <li className={`flex align-items-end`}>
           <Link href={`/`} className={`${styles.logo}`}>
-            <svg width="40" height="40" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <rect width="40" height="40" rx="20" fill="black" />
-              <path fillRule="evenodd" clipRule="evenodd" d="M13.7541 28.2442C17.1945 22.1137 25.2621 23.9693 26.5435 30H29.5C27.5791 23.3766 22.5435 20.6087 16.2826 22.5217L13.7541 28.2442Z" fill="white" />
-              <path d="M19 10L10 30H12.9783C13.198 29.3544 13.459 28.7699 13.7541 28.2442L16.2826 22.5217L21.6739 10H19Z" fill="white" />
-            </svg>
+            <Image src={logo} alt={`${process.env.NEXT_PUBLIC_NAME} Logo`} width={32} height={32} />
           </Link>
         </li>
         <li className={`flex justify-content-end align-items-center gap-050`}>
