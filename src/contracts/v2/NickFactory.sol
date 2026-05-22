9e3788eeef41d5a56d120b15de2be491b769f846
web3senior <atenyun@gmail.com>
2026-05-22 04:18:31 +0330

    refactor: remove unused error and event definitions from contracts

--- src/contracts/v2/NickFactory.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
-
-interface INickFactory {
-    // The standard Nick's Factory deployment function
-    // It expects the salt followed immediately by the bytecode
-    function deploy(bytes32 salt, bytes calldata bytecode) external returns (address);
-}
