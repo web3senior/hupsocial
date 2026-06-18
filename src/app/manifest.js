8a8df50a440f0361b5d12a1696ae803aa353c7b6
web3senior <atenyun@gmail.com>
2026-06-18 23:51:27 +0330

    fix: change background color to black in manifest

--- src/app/manifest.js
+++ src/app/manifest.js
@@ -5,7 +5,7 @@ export default function manifest() {
     description: 'Fully onchain social network',
     start_url: '/',
     display: 'standalone',
-    background_color: '#ffffff',
+    background_color: '#000000',
     theme_color: '#000000',
     icons: [
       {
