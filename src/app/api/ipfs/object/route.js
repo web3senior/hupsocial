aa5f2ab9679c3738b9c4406c9886d228bce0d4c2
web3senior <atenyun@gmail.com>
2026-06-29 17:05:24 +0330

    feat: implement IPFS JSON upload API route using Pinata SDK

--- src/app/api/ipfs/object/route.js
+++ src/app/api/ipfs/object/route.js
@@ -40,7 +40,7 @@ export async function POST(request) {
 
     // Use the Pinata 'json' upload method.
     const { cid: rawCID } = await pinata.upload.public.json(json, {
-      pinataMetadata: { name: `hup-metadata` },
+      pinataMetadata: { name: `metadata` },
     })
 
     const cid = `ipfs://${rawCID}`
